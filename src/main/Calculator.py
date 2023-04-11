96187ccfd51a545bf28bac61fbe6cbecc547e1b1
Varsha Singh <32224935+Varshasingh214@users.noreply.github.com>
2023-04-11 18:02:19 -0400

    calculator simple

---
+++ src/main/Calculator.py
@@ -0,0 +1,13 @@
+
+class Calculator():
+    def add(a,b):
+        return a+b
+
+    def cross(a,b):
+        return a*b
+
+obj1 = Calculator.add(2,3)
+print(obj1)
+obj2 = Calculator.cross(2,3)
+print(obj2)
+
