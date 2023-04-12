8687038d2d450c6fdccf40a785b645243de3f763
Varsha Singh <32224935+Varshasingh214@users.noreply.github.com>
2023-04-11 21:27:59 -0400

    practising to do stash

--- src/main/Calculator.py
+++ src/main/Calculator.py
@@ -6,8 +6,12 @@ class Calculator():
     def cross(a,b):
         return a*b
 
+    def divide(a,b):
+        return b/a
+
 obj1 = Calculator.add(2,3)
 print(obj1)
 obj2 = Calculator.cross(2,3)
 print(obj2)
-
+obj3 = Calculator.divide(50,5)
+print(obj3)
